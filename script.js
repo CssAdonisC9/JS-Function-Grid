7391509f59cc062affc41f7cc988f3659ebcc973
Adonis Mateo <amatt2000@hotmail.com>
2017-02-27 13:22:15 +0000

    I changed the java

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //Write your function calls below this line
+setColor(0, 0, "red");
+setColor(0, 9, "red");
+setColor(9, 0, "red");
+setColor(9, 9, "red");
+
+setColor(3, 3, "blue");
+setColor(3, 5, "blue");
+setColor(4, 1, "blue");
+setColor(5, 2, "blue");
+setColor(5, 3, "blue");
+setColor(5, 4, "blue");
+setColor(5, 5, "blue");
+setColor(5, 6, "blue");
+setColor(4, 7, "blue");
